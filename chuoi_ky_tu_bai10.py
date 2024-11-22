6c7a791297acad33bfa855d684b2c1d6ef1437d3
daoyenly <dyly.24174600101@sv.uneti.edu.vn>
2024-11-22 11:20:17 +0700

    fksgjkf

---
+++ chuoi_ky_tu_bai10.py
@@ -0,0 +1,7 @@
+input_string = input("Nhập vào chuỗi ký tự: ")
+
+chu_so = ''.join(char for char in input_string if char.isdigit())  
+chu_cai = ''.join(char for char in input_string if not char.isdigit())
+
+cong_tong = chu_so + chu_cai  
+print(f"Chuỗi mới sau khi dồn số sang bên trái: {cong_tong}")
